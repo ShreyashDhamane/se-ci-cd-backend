e966e6e3c5d3443e6f749c7d40afb0f86c8a59ef
shreyash dhamane <shreyashb.dhamane0@gmail.com>
2022-11-21 23:20:51 +0530

    'as'

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.json());
 const port = 8000;
 
+app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+});
+
 app.post("/login", (req, res, err) => {
   if (err) {
     //
